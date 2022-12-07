f7d47696c65ead0cae5ffc5d816df4456f91aa14
Chege Ian <chegeian65@gmail.com>
2022-12-07 22:12:01 +0300

    done

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,5 +1,6 @@
 class AuthorSerializer < ActiveModel::Serializer
   attributes :name
-  has_one :profile, serializer: AuthorProfileSerializer
+
+  has_one :profile
   has_many :posts, serializer: AuthorPostSerializer
 end
